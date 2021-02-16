8ec68b3491bbedad6e5b1dc677e0cd203dbc49dc
BharatD-dev <75020046+BharatD-dev@users.noreply.github.com>
2021-02-16 12:13:34 +0530

    youtuber-meet Uploaded

--- tubers/settings.py
+++ tubers/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -142,3 +143,4 @@ STATICFILES_DIRS = [
 
 
 SITE_ID = 1
+django_heroku.settings(locals())
